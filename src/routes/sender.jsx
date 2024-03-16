197ca8dfb9ca276e6f57dae3c57e77b4c7a4f073
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-03-16 17:34:11 +0530

    feat: analytics completed

--- src/routes/sender.jsx
+++ src/routes/sender.jsx
@@ -2,12 +2,15 @@
 import * as React from 'react'
 import {createFileRoute, Link, Outlet, useParams} from '@tanstack/react-router'
 import {Box, Button, Flex, Text} from "@chakra-ui/react";
+import {useEffect} from "react";
+import {get} from "../request.js";
 
 export const Route = createFileRoute('/sender')({
     component: LayoutComponent,
 })
 
 function LayoutComponent() {
+    const [agentName, setAgentName] = React.useState("")
     const senderId = useParams({
         from: '/sender/$id', select: (params) => params.id,
     })
@@ -16,13 +19,33 @@ function LayoutComponent() {
         return <Button colorScheme={isActive ? "teal" : "gray"} rounded="full">{label}</Button>
     }
 
+    function fetchAgentName() {
+        get(`/api/management/senders/${senderId}`, {})
+            .then((data) => {
+                console.log(data)
+                if (data.type === "APP") {
+                    setAgentName(data.applicationName);
+                } else {
+                    setAgentName(data.hostname);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchAgentName();
+    }, [senderId]);
+
     return (<Box p={10} pt={0}>
         <Flex flexDirection="row" gap={3} mb={5}>
-            <Text mr={6} fontSize="2xl" fontWeight="bold">{senderId}</Text>
-            <Link to={`/sender/${senderId}/analytics`} children={({isActive}) => linkbutton("Analytics", isActive)} />
-            <Link to={`/sender/${senderId}/logs`} children={({isActive}) => linkbutton("Collected Logs", isActive)} />
-            <Link to={`/sender/${senderId}/crashlytics`} children={({isActive}) => linkbutton("Crashlytics", isActive)}/>
-            <Link to={`/sender/${senderId}/issues`} children={({isActive}) => linkbutton("Issue Reporting", isActive)} />
+            <Text mr={3} fontSize="2xl" fontWeight="bold">{agentName}</Text>
+            <Link to={`/sender/${senderId}/analytics`} children={({isActive}) => linkbutton("Analytics", isActive)}/>
+            <Link to={`/sender/${senderId}/logs`} children={({isActive}) => linkbutton("Collected Logs", isActive)}/>
+            <Link to={`/sender/${senderId}/crashlytics`}
+                  children={({isActive}) => linkbutton("Crashlytics", isActive)}/>
+            <Link to={`/sender/${senderId}/issues`} children={({isActive}) => linkbutton("Issue Reporting", isActive)}/>
         </Flex>
         <Outlet/>
     </Box>)

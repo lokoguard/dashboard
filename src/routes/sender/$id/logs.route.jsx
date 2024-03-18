a88742a5389120ffac5c6f31f7c86ed714a397af
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-03-18 12:57:27 +0530

    feat: log page completed

--- src/routes/sender/$id/logs.route.jsx
+++ src/routes/sender/$id/logs.route.jsx
@@ -1,95 +1,129 @@
-import {createFileRoute, useParams} from "@tanstack/react-router";
+import { createFileRoute, useParams } from "@tanstack/react-router";
 import DataTable from "react-data-table-component";
 // TODO: delete it
-import {faker} from '@faker-js/faker';
-import {useEffect, useMemo, useState} from "react";
-import {Box, Flex, Input} from "@chakra-ui/react";
-import {get} from "../../../request.js";
+import { faker } from "@faker-js/faker";
+import { useEffect, useMemo, useState } from "react";
+import { Box, Flex, Input } from "@chakra-ui/react";
+import { get } from "../../../request.js";
 import toast from "react-hot-toast";
+import { getFormattedDateTime, getRelativeTime } from "../../../utils.jsx";
 
 export const Route = createFileRoute("/sender/$id/logs")({
-    component: Index,
+  component: Index,
 });
 
 const createUser = () => ({
-    id: faker.string.uuid(),
-    name: faker.internet.userName(),
-    email: faker.internet.email(),
-    address: faker.location.streetAddress(),
-    bio: faker.lorem.sentence(),
-    image: faker.image.avatar(),
+  id: faker.string.uuid(),
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  address: faker.location.streetAddress(),
+  bio: faker.lorem.sentence(),
+  image: faker.image.avatar(),
 });
 
-const createUsers = (numUsers = 5) => new Array(numUsers).fill(undefined).map(createUser);
-
-const fakeUsers = createUsers(2000);
-
 export default function Index() {
-    const senderId = useParams({
-        from: '/sender/$id', select: (params) => params.id,
-    })
-    const [logs, setLogs] = useState([])
+  const senderId = useParams({
+    from: "/sender/$id",
+    select: (params) => params.id,
+  });
+  const [logs, setLogs] = useState([]);
 
-    const columns = [{
-        name: 'Name', selector: row => row.name, sortable: true,
-    }, {
-        name: 'Email', selector: row => row.email, sortable: true,
-    }, {
-        name: 'Address', selector: row => row.address, sortable: true,
-    },];
-    const [filterText, setFilterText] = useState('');
-    const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
-    const filteredItems = fakeUsers.filter(item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()),);
+  const columns = [
+    {
+      name: "Facility Level",
+      selector: (row) => row.facilityLevel,
+      sortable: true,
+    },
+    {
+      name: "Severity Level",
+      selector: (row) => row.severityLevel,
+      sortable: true,
+    },
+    {
+      name: "Appname",
+      selector: (row) => row.appname,
+      sortable: true,
+    },
+    {
+      name: "Timestamp",
+      selector: (row) => getFormattedDateTime(row.timestamp * 1000),
+      sortable: true,
+    },
+  ];
+  const [filterText, setFilterText] = useState("");
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+  const filteredItems = logs.filter(
+    (item) =>
+      item.message &&
+      item.message.toLowerCase().includes(filterText.toLowerCase()),
+  );
 
-    const subHeaderComponentMemo = useMemo(() => {
-        const handleClear = () => {
-            if (filterText) {
-                setResetPaginationToggle(!resetPaginationToggle);
-                setFilterText('');
-            }
-        };
+  const subHeaderComponentMemo = useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText("");
+      }
+    };
 
-        return (<FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear}
-                                 filterText={filterText}/>);
-    }, [filterText, resetPaginationToggle]);
+    return (
+      <FilterComponent
+        onFilter={(e) => setFilterText(e.target.value)}
+        onClear={handleClear}
+        filterText={filterText}
+      />
+    );
+  }, [filterText, resetPaginationToggle]);
 
+  const expandableRowsComponent = ({ data }) => {
+    return (
+      <Box p={3} mt={4} bg="gray.100" rounded="md">
+        {data.message}
+      </Box>
+    );
+  };
 
-    const expandableRowsComponent = ({data}) => {
-        console.log(data);
-        return <Box>Extra info</Box>;
-    }
+  const fetchLogs = async () => {
+    get(`/api/management/senders/${senderId}/logs`, {})
+      .then((result) => {
+        setLogs(result);
+        console.log(result);
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
-    const fetchLogs = async () => {
-        get(`/api/management/senders/${senderId}/logs`, {}).then((result) => {
-            setLogs(result);
-            console.log(result);
-        }).catch((error) => {
-            toast.error(error.message);
-        })
-    }
+  useEffect(() => {
+    fetchLogs();
+  }, []);
 
-    useEffect(() => {
-        fetchLogs();
-    }, []);
-
-    return <div>
-
-        <DataTable
-            columns={columns}
-            data={filteredItems}
-            pagination
-            paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
-            subHeader
-            subHeaderComponent={subHeaderComponentMemo}
-            expandableRows
-            expandableRowsComponent={expandableRowsComponent}
-            persistTableHead
-        />
-
-    </div>;
+  return (
+    <div>
+      <DataTable
+        columns={columns}
+        data={filteredItems}
+        pagination
+        paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
+        expandableRows
+        expandableRowsComponent={expandableRowsComponent}
+        persistTableHead
+        paginationPerPage={10}
+      />
+    </div>
+  );
 }
 
 // eslint-disable-next-line react/prop-types
-const FilterComponent = ({filterText, onFilter, onClear}) => (<Flex flexDirection="row">
-    <Input type='text' value={filterText} onChange={onFilter} placeholder="Filter"/>
-</Flex>);
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  <Flex flexDirection="row">
+    <Input
+      type="text"
+      value={filterText}
+      onChange={onFilter}
+      placeholder="Filter"
+    />
+  </Flex>
+);
--- src/utils.jsx
+++ src/utils.jsx
@@ -1,83 +1,80 @@
 import moment from "moment";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function formEventToJson(event) {
-    const data = new FormData(event.target);
-    const json = {};
-    data.forEach((value, key) => {
-            json[key] = value;
-        }
-    );
-    return json;
+  const data = new FormData(event.target);
+  const json = {};
+  data.forEach((value, key) => {
+    json[key] = value;
+  });
+  return json;
 }
 
 function getRelativeTime(dateTimeStr) {
-    const currentTime = moment();
-    const pastTime = moment(dateTimeStr);
-    const duration = moment.duration(currentTime.diff(pastTime));
+  const currentTime = moment();
+  const pastTime = moment(dateTimeStr);
+  const duration = moment.duration(currentTime.diff(pastTime));
 
-    const years = duration.years();
-    const months = duration.months();
-    const days = duration.days();
-    const hours = duration.hours();
-    const minutes = duration.minutes();
-    const seconds = duration.seconds();
+  const years = duration.years();
+  const months = duration.months();
+  const days = duration.days();
+  const hours = duration.hours();
+  const minutes = duration.minutes();
+  const seconds = duration.seconds();
 
-    if (years > 0) {
-        return `${years}y ago`;
-    } else if (months > 0) {
-        return `${months}mo ago`;
-    } else if (days > 0) {
-        return `${days}d ago`;
-    } else if (hours > 0) {
-        return `${hours}hr ago`;
-    } else if (minutes > 0) {
-        return `${minutes}m ago`;
-    } else {
-        return `${seconds}s ago`;
-    }
+  if (years > 0) {
+    return `${years}y ago`;
+  } else if (months > 0) {
+    return `${months}mo ago`;
+  } else if (days > 0) {
+    return `${days}d ago`;
+  } else if (hours > 0) {
+    return `${hours}hr ago`;
+  } else if (minutes > 0) {
+    return `${minutes}m ago`;
+  } else {
+    return `${seconds}s ago`;
+  }
 }
 
 function isOnline(dateTimeStr) {
-    const currentTime = moment();
-    const pastTime = moment(dateTimeStr);
-    const duration = moment.duration(currentTime.diff(pastTime));
-    return duration.asMinutes() < 5;
+  const currentTime = moment();
+  const pastTime = moment(dateTimeStr);
+  const duration = moment.duration(currentTime.diff(pastTime));
+  return duration.asMinutes() < 5;
 }
 
+function RelativeTimeDisplay({ dateTimeStr }) {
+  const [relativeTime, setRelativeTime] = useState("");
 
-function RelativeTimeDisplay({dateTimeStr}) {
-    const [relativeTime, setRelativeTime] = useState('');
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRelativeTime(getRelativeTime(dateTimeStr));
+    }, 1000); // Update every second
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setRelativeTime(getRelativeTime(dateTimeStr));
-        }, 1000); // Update every second
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, [dateTimeStr]);
 
-        return () => clearInterval(interval); // Clean up the interval on unmount
-    }, [dateTimeStr]);
-
-    return <>{relativeTime}</>;
+  return <>{relativeTime}</>;
 }
 
+function getFormattedDateTime(timestamp) {
+  const currentDateTime = new Date(timestamp);
+  const year = currentDateTime.getFullYear();
+  const month = String(currentDateTime.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDateTime.getDate()).padStart(2, "0");
+  const hours = String(currentDateTime.getHours()).padStart(2, "0");
+  const minutes = String(currentDateTime.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12;
 
-function getFormattedDateTime() {
-    const currentDateTime = new Date();
-    const year = currentDateTime.getFullYear();
-    const month = String(currentDateTime.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDateTime.getDate()).padStart(2, '0');
-    const hours = String(currentDateTime.getHours()).padStart(2, '0');
-    const minutes = String(currentDateTime.getMinutes()).padStart(2, '0');
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    const formattedHours = hours % 12 || 12;
-
-    return `${month}-${day}-${year} ${formattedHours}:${minutes}${ampm}`;
+  return `${month}-${day}-${year} ${formattedHours}:${minutes}${ampm}`;
 }
 
 export {
-    formEventToJson,
-    getRelativeTime,
-    RelativeTimeDisplay,
-    isOnline,
-    getFormattedDateTime,
-}
+  formEventToJson,
+  getRelativeTime,
+  RelativeTimeDisplay,
+  isOnline,
+  getFormattedDateTime,
+};

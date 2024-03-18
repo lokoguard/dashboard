ea8ae7527945c2e968cc3db49d1587b9d499bb26
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-03-18 14:27:06 +0530

    feat: blockly boilerplate added

--- src/routes/policy/$id/designer.route.jsx
+++ src/routes/policy/$id/designer.route.jsx
@@ -1,18 +1,56 @@
-import {createFileRoute} from "@tanstack/react-router";
-import {BlocklyWorkspace} from "react-blockly";
+import { createFileRoute } from "@tanstack/react-router";
+import { BlocklyWorkspace } from "react-blockly";
+
 import toolbox from "../../../blockly/toolbox.js";
+import { useRef, useState } from "react";
+import { Button } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import generateCode from "../../../blockly/generator.js";
 
 export const Route = createFileRoute("/policy/$id/designer")({
-    component: Index,
+  component: Index,
 });
 
-
 function Index() {
-    return <div className="blockly-container">
-        <BlocklyWorkspace
-            className="editor"
-            toolboxConfiguration={toolbox}
-        />
-    </div>
+  const [initialJson, setInitialJson] = useState({});
+  const workspaceRef = useRef(null);
 
+  const jsonUpdated = (json) => {
+    setInitialJson(json);
+  };
+
+  const generateAndSaveCode = () => {
+    if (workspaceRef.current) {
+      const code = generateCode(workspaceRef.current);
+      console.log(code);
+    }
+  };
+
+  return (
+    <>
+      <div className="blockly-container">
+        <BlocklyWorkspace
+          className="editor"
+          onWorkspaceChange={(workspace) => (workspaceRef.current = workspace)}
+          initialJson={initialJson}
+          onJsonChange={jsonUpdated}
+          toolboxConfiguration={toolbox}
+        />
+        {/*  Floating button to save  */}
+        <Button
+          size="md"
+          colorScheme="teal"
+          position="absolute"
+          right={10}
+          top={5}
+          zIndex={100}
+          leftIcon={<FontAwesomeIcon icon={faSave} />}
+          onClick={generateAndSaveCode}
+        >
+          Save Policy
+        </Button>
+      </div>
+    </>
+  );
 }
